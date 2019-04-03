4eb4d0349d5d814acd0b21a545549e1160be3bd1
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 18:30:11 +0200

    added scale fill exercises

--- exercises/exercise_08_scales_colfill.R
+++ exercises/exercise_08_scales_colfill.R
@@ -9,7 +9,7 @@ a <- ggplot(
     x = reorder(month_name, month),
     y = enterococci,
     fill = month
-    )
+  )
 ) +
   geom_violin() +
   scale_y_continuous(
@@ -19,10 +19,51 @@ a <- ggplot(
     limits = c(.1, 1000)
   )
 
-a +
-  scale_fill_continuous(
-    name = "Month",
-    type = "scico"
+# the default "scale_fill_continuous" has a
+# variety of palettes that you can specify with
+# "type"
+a + scale_fill_continuous(
+  name = "Month",
+  type = "viridis"
+)
+
+# but there are other ways to do the same thing:
+a + scale_fill_viridis_c(
+  name = "Month"
+)
+
+# lots of packages out there. scico is one of my
+# favourites so I'll use that
+a + scale_fill_scico(palette = "tokyo")
+
+
+# previous version was set up to treat month as
+# a continuous variable (which, yeah okay). I did
+# this by linking the fill aesthetic to the numeric
+# month variable. Now let's do the same thing with
+# the month_name:
+
+
+b <- ggplot(
+  data = beaches,
+  mapping = aes(
+    x = reorder(month_name, month),
+    y = enterococci,
+    fill = reorder(month_name, month)
   )
+) +
+  geom_violin() +
+  scale_x_discrete(name = "Month") +
+  scale_y_continuous(
+    name = "Enterococci (cfu/100ml)",
+    trans = "log10",
+    breaks = c(1, 10, 100),
+    limits = c(.1, 1000)
+  )
+
+b + scale_fill_discrete(name = "Month")
+b + scale_fill_scico_d(name = "tokyo")
+
+
 
 
