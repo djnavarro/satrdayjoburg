79435958dfee6672d1ac27bb1102c0a1a8702177
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 16:17:21 +0200

    tidied data sets

--- other/make_beaches2.R
+++ other/make_beaches2.R
@@ -1,14 +1,12 @@
 library(tidyverse)
-library(lubridate)
-long_beaches <- read_csv(here::here("data","sydneybeaches2.csv"))
 
-beaches <- long_beaches %>%
-  group_by(date, year, month, day, season) %>%
-  summarise(
-    rainfall = mean(rainfall),
-    temperature = mean(temperature),
-    enterococci = mean(enterococci)
-  ) %>% ungroup()
+raw_beaches <- read_csv(here::here("data","sydneybeaches1.csv"))
+
+beaches <- raw_beaches %>%
+  rename(enterococci = beachbugs,
+         rainfall = rain_mm,
+         temperature = temp_airport
+        )
 
 day_of_year <- function(date, year) {
   1+ as.numeric(date -lubridate::dmy(paste("1 1",year)))
@@ -27,5 +25,4 @@ beaches <- beaches %>%
       month %in% 9:11 ~ 4)
   )
 
-beaches %>% write_csv(here::here("data","sydneybeaches3.csv"))
-
+beaches %>% write_csv(here::here("data","sydneybeaches2.csv"))
