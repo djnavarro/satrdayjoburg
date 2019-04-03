0905111077e10d348634d426242933089cc3b6be
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 20:26:05 +0200

    tidied exercise 4

--- solutions/solutions_04_layers.R
+++ solutions/solutions_04_layers.R
@@ -43,12 +43,43 @@ c <- ggplot(beaches, aes(temperature, rainfall)) +
 #### YOUR-CODE-HERE ####
 
 
+# Solution 1:
 
+d <- ggplot() +
+  layer(
+    data = beaches,
+    mapping = aes(
+      x = temperature,
+      y = rainfall),
+    geom = "point",
+    stat = "identity",
+    position = "identity",
+    params = list(size = 3)
+  ) +
+  layer(
+    data = beaches,
+    mapping = aes(
+      x = temperature,
+      y = rainfall,
+      colour = season_name),
+    geom = "rug",
+    stat = "identity",
+    position = "identity"
+  ) +
+  scale_x_continuous(name = "Temperature") +
+  scale_y_continuous(name = "Rainfall") +
+  theme_grey() +
+  facet_null() +
+  coord_cartesian()
 
 
 # Exercise 2: Why is it useful to specify data and mappings
 # globally in the the ggplot() function?
 
+
+# Solution 2: The beaches data are used in both layers,
+# as are the x and y aesthetics. The expanded version is
+# very long.
 
 
 
