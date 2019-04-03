c7538a1254e2e1222405243e5a626e4fbba8ebe8
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 15:50:16 +0200

    added themes exercises

--- exercises/exercise_10_themes.R
+++ exercises/exercise_10_themes.R
@@ -3,3 +3,82 @@ library(here)
 beaches <- read_csv(here("data","sydneybeaches3.csv"))
 
 
+# Base plot
+a <- ggplot(
+  data = beaches,
+  mapping = aes(
+    x = reorder(month_name, month),
+    y = enterococci)
+) +
+  geom_violin() +
+  scale_y_continuous(
+    name = "Enterococci (cfu/100ml)",
+    trans = "log10",
+    breaks = c(1, 10, 100),
+    limits = c(.1, 1000)
+  ) +
+  scale_x_discrete(name = "Month") +
+  ggtitle("Enterococci by Month")
+
+# Global changes to the theme
+a + theme_bw()
+a + theme_minimal()
+a + theme_void()
+a + theme_dark()
+
+# To tinker, recall we have four basic element types:
+#  - element_line (e.g. axis lines, tick marks, grid lines, etc)
+#  - element_text (e.g. axis text, titles, legend text etc)
+#  - element_rect (e.g. background, legend boxes, facet strips etc)
+#  - element_blank (anything we want to remove)
+
+# Let's make the a variation of theme dark by sequentially modifying
+# different elements from theme_grey(). Frist, make the panel
+# background darker
+a + theme(
+  panel.background = element_rect(fill = "grey40")
+)
+
+# Now let's make the plot background darker as well
+a + theme(
+  panel.background = element_rect(fill = "grey40"),
+  plot.background = element_rect(fill = "grey65")
+)
+
+# Those grid lines (within the panel) are too white now,
+# so lets have them blend into the plot background:
+b <- a + theme(
+  panel.background = element_rect(fill = "grey40"),
+  plot.background = element_rect(fill = "grey65"),
+  panel.grid = element_line(colour = "grey65")
+)
+
+# The text is a little small. We can tinker with individual
+# text elements (title labels etc, but we can set them all
+# at once like this)
+b + theme(
+  text = element_text(size = 14, colour = "white")
+)
+
+# Notice which ones change and which don't. To modify the
+# axis.text separately...
+c <- b + theme(
+  text = element_text(size = 14, colour = "white"),
+  axis.text = element_text(size = 14, colour = "white")
+)
+
+# Now we have a problem: the x-axis text overlaps. Again
+# these are text elemenets so...
+c + theme(
+  axis.text.x = element_text(angle = 30)
+)
+
+# Okay that's a start but now look at the text justification
+# By default they're center justified, and we need to make it
+# right justified:
+d <- c + theme(
+  axis.text.x = element_text(angle = 30, hjust = 1)
+)
+
+# Oh, and let's save this last plot
+ggsave(here::here("output","darkbugs.png"))
