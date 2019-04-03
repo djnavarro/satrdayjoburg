1534d9402ba42b955ec13ab12875a66b00386285
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 18:20:21 +0200

    added scico

--- exercises/exercise_08_scales_colfill.R
+++ exercises/exercise_08_scales_colfill.R
@@ -1,5 +1,28 @@
 library(tidyverse)
 library(here)
+library(scico)
 beaches <- read_csv(here("data","sydneybeaches3.csv"))
+
+a <- ggplot(
+  data = beaches,
+  mapping = aes(
+    x = reorder(month_name, month),
+    y = enterococci,
+    fill = month
+    )
+) +
+  geom_violin() +
+  scale_y_continuous(
+    name = "Enterococci (cfu/100ml)",
+    trans = "log10",
+    breaks = c(1, 10, 100),
+    limits = c(.1, 1000)
+  )
+
+a +
+  scale_fill_continuous(
+    name = "Month",
+    type = "scico"
+  )
 
 
