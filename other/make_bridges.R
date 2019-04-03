1d6d740fe376beaa439fa983a999e414f89afd2d
Danielle Navarro <d.navarro@unsw.edu.au>
2019-04-03 12:40:25 +0200

    created the bridges data

---
+++ other/make_bridges.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(e1071)
+
+ntimes <- 20  # how many time points to run the bridge?
+nseries <- 10 # how many time series to generate?
+
+# function to generate the brownian bridges
+make_bridges <- function(ntimes, nseries) {
+  replicate(nseries, c(0,rbridge(frequency = ntimes-1))) %>% as.vector()
+}
+
+# construct tibble
+tbl <- tibble(
+  Time = rep(1:ntimes, nseries),
+  Horizontal = make_bridges(ntimes, nseries),
+  Vertical = make_bridges(ntimes, nseries),
+  Series = gl(nseries, ntimes)
+)
+
+# write it
+tbl %>% write_csv(here::here("data","brownian_bridges.csv"))
